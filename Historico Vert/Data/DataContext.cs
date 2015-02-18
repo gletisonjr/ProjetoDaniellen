36b02cca877fff91e05279ed23282921b7297440
felirey <felirey.ramos@gmail.com>
2015-02-18 10:36:10 -0200

    Repositório genérico
    Métodos do repositório

--- Historico Vert/Data/DataContext.cs
+++ Historico Vert/Data/DataContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Historico_Vert.Data
@@ -18,25 +20,82 @@ namespace Historico_Vert.Data
             if (persistentObject == null)
                 return "Não foi possível salvar. Os campos estão vazios";
 
-
             using (var context = new clinicaEntities())
             {
-                // Obtem o nome em String do tipo da Entidade.
-                String nomeEntidade = context.GetType().GetProperties().
-                    Single(
-                        s =>
-                            s.PropertyType.IsGenericType &&
-                            typeof (IEnumerable<>).MakeGenericType(typeof (T)).IsAssignableFrom(s.PropertyType))
-                    .Name;
+                var nomeEntidade = NomeEntidade<T>(context);
 
                 context.AddObject(nomeEntidade, persistentObject);
                 context.SaveChanges();
-
             }
 
             return "Salvo com sucesso";
         }
 
-  
+        /// <summary>
+        /// Método responsável por obter o nome de uma Entidade.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private static string NomeEntidade<T>(clinicaEntities context)
+        {
+            // Obtem o nome em String do tipo da Entidade.
+            String nomeEntidade = context.GetType().GetProperties().
+                Single(
+                    s =>
+                        s.PropertyType.IsGenericType &&
+                        typeof(IEnumerable<>).MakeGenericType(typeof(T)).IsAssignableFrom(s.PropertyType))
+                .Name;
+
+            return nomeEntidade;
+        }
+
+
+        public static String Delete<T>(T persistentObject)
+        {
+            if (persistentObject == null)
+                return "Não foi possível realizar a exclusão deste registro";
+
+            using (var context = new clinicaEntities())
+            {
+                context.DeleteObject(persistentObject);
+                context.SaveChanges();
+            }
+
+            return "Deletado com sucesso";
+        }
+
+        public static String Delete<T>(Expression<Func<T, bool>> predicate)
+        {
+            using (var context = new clinicaEntities())
+            {
+                var deleteList = context.CreateQuery<T>(NomeEntidade<T>(context)).Where(predicate).ToList();
+
+                if (!deleteList.Any())
+                    return "Não há registros para deletar";
+
+                context.DeleteObject(deleteList);
+                context.SaveChanges();
+            }
+
+            return "Registro(s) deletados com sucesso";
+        }
+
+
+        public static List<String> AutoCompleteLista<T>(Expression<Func<T, bool>> predicate, Expression<Func<T, String>> selecao)
+        {
+            using (var context = new clinicaEntities())
+                return context.CreateQuery<T>(NomeEntidade<T>(context)).Where(predicate).Take(5).Select(selecao).ToList();
+        }
+
+
+        public static T Obter<T>(Expression<Func<T, bool>> predicate)
+        {
+            using (var context = new clinicaEntities())
+                return context.CreateQuery<T>(NomeEntidade<T>(context)).FirstOrDefault(predicate);
+            
+        }
+
+
     }
 }
